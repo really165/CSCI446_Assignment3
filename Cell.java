a3a329ef3d6da63ffc9f970e389676657f94c908
really165 <31826004+really165@users.noreply.github.com>
2018-11-30 13:37:30 -0700

    constructor so Cell can hold a coordinate and time

--- Cell.java
+++ Cell.java
@@ -5,6 +5,15 @@ public class Cell{
     boolean wumpus = false, pit = false, gold = false;
     boolean stench = false, breeze = false, glitter = false;
     
+    int row,column,time;
+    
     public Cell(){
+    }
+    
+    //constructor with row, column, and time
+    public Cell(int r, int c, int t){
+        this.row = r;
+        this.column = c;
+        this.time = t;
     }
 }
